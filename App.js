c6bda412f36bb328d3b36bbc9006c257c8029d64
Admın <admin@Admns-MacBook-Air.local>
2019-12-24 09:43:12 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{alignItems:"center",backgroundColor:'green', alignContent:'center', paddingTop:300}}>
+      <Text style={{color:'red'}}>Hello!</Text>
+    </View>
+  );
+}
